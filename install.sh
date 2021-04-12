661a68bccac20d960b9449bdc1e9ac1a8f6c25ff
Walmes Zeviani <walmes@ufpr.br>
2021-04-12 17:03:21 -0300

    Adds a shell file with some instructions.

---
+++ install.sh
@@ -0,0 +1,51 @@
+#-----------------------------------------------------------------------
+# Install Doom-Emacs and add personal configuration.
+
+# Keep a copy of `.emacs.d/`.
+mv -v ~/.emacs.d ~/OLD.emacs.d/
+
+# Install Doom-Emacs.
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+# Brings my persional configuration.
+rm -rf ~/.doom.d/
+git clone git@github.com:walmes/doom-emacs.git ~/.doom.d/
+
+# Upgrade Doom-emacs.
+~/.emacs.d/bin/doom upgrade
+
+#-----------------------------------------------------------------------
+# Install some packages manually.
+
+# (require 'package)
+# (package-initialize)
+# (when (not (package-installed-p 'electric-spacing-r))
+#   (url-copy-file
+#    "https://raw.githubusercontent.com/walmes/electric-spacing/master/electric-spacing-r.el"
+#    "~/.doom.d/electric-spacing-r.el")
+#   (byte-compile-file "~/.doom.d/electric-spacing-r.el"))
+# (when (not (package-installed-p 'essh))
+#   (url-copy-file
+#    "https://www.emacswiki.org/emacs/download/essh.el"
+#    "~/.doom.d/essh.el")
+#   (byte-compile-file "~/.doom.d/essh.el"))
+
+# Requirements for `python` modele.
+# https://github.com/hlissner/doom-emacs/tree/develop/modules/lang/python
+#   cd anaconda
+#   source activate
+#   pip install pytest nose black pyflakes isort
+#   pip install python-language-server[all]
+#   pip install pyright
+#   conda deactivate
+# On Emacs, after refreshing it:
+#   M-x lsp-install-server RET mspyls
+# https://github.com/microsoft/pyright
+
+# Requirements for `ess` module.
+LIB="/usr/lib/R/site-library"
+REPOS="http://cran-r.c3sl.ufpr.br/"
+PKG="lintr"
+CMD="lib <- \"$LIB\"; repos <- \"$REPOS\"; install.packages(\"$PKG\", dependencies = TRUE, lib = lib, repos = repos)"
+sudo Rscript -e "$CMD"
