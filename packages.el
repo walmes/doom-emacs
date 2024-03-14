a6e8f8c49947d738a02b00066bd145ff2c106d96
Walmes Zeviani <walmes@ufpr.br>
2024-03-14 11:31:06 -0400

    Organize packages and describe them.

--- packages.el
+++ packages.el
@@ -53,41 +53,73 @@
 ;; https://tapoueh.org/blog/2010/08/el-get/
 ;; (package! el-get)
 
-(package! magit)
+;; Visible bookmarks in buffer for GNU Emacs. Provides an easy way to
+;; navigate in a buffer.
+;; https://github.com/joodland/bm
 (package! bm)
-(package! yafolding)
-(package! smartparens)
-(package! markdown-mode)
-(package! orgalist)
-(package! elpy)
-(package! imenu-list)
-(package! auctex)
-(package! ess-view-data)
-(package! lsp-treemacs)
-(package! hl-prog-extra)
-(package! yasnippet)
-(package! quarto-mode)
 
+;; Bookmark+ significantly enhances the bookmark management experience
+;; in Emacs by providing a comprehensive set of features for creating,
+;; organizing, annotating, and navigating bookmarks.
+;; https://github.com/emacsmirror/bookmark-plus
+(package! bookmark+
+  :recipe (:host github
+           :repo "emacsmirror/bookmark-plus"))
+
+;; Folding code blocks based on indentation.
+;; https://github.com/zenozeng/yafolding.el
+(package! yafolding)
+
+;; Ease navigation in the buffer with list for language objects or
+;; document sections.
+;; https://github.com/bmag/imenu-list
+(package! imenu-list)
+
+;; This package provides an easy way to highlight words in programming
+;; modes, where terms can be highlighted on code, comments or strings.
+;; https://codeberg.org/ideasman42/emacs-hl-prog-extra
+(package! hl-prog-extra)       ;; Complements `:ui hl-todo'.
+
+;; ANSI, XTERM 256 and Truecolor support.
+;; No probles with colors in the terminal.
+;; https://github.com/atomontage/xterm-color
+(package! xterm-color)
+
+;; Provides a wide range of attractive icons that represent different
+;; file types, directories, and special symbols, enriching the user
+;; interface and making it more intuitive and visually appealing.
+;; https://github.com/domtronn/all-the-icons.el
 (package! all-the-icons)
 ;; TODO M-x all-the-icons-install-fonts
 
+;; Nerd-icons.el is a library for easily using Nerd Font icons inside
+;; Emacs, an alternative to all-the-icons. It works on both GUI and
+;; terminal! You only need a Nerd Font installed on your system.
+;; https://github.com/rainstormstudio/nerd-icons.el
 (package! nerd-icons)
 ;; TODO M-x nerd-icons-install-fonts
 
+;; `nerd-icons' theme for `treemacs'. It can be used inside GUI or
+;; terminal.
+;; https://github.com/rainstormstudio/treemacs-nerd-icons
 (package! treemacs-nerd-icons :pin "9876cb478145a0ec4e36f64ff6583f3de7126216")
 
-;; https://github.com/jojojames/dired-sidebar/blob/master/dired-sidebar.el
+;; The Dired Sidebar package enriches the Emacs experience by providing
+;; a user-friendly sidebar interface for file management tasks, with
+;; navigation and Dired integration.
+;; https://github.com/jojojames/dired-sidebar
 (package! dired-sidebar)
 
+;; `all-the-icons-dired' significantly improves the visual
+;; representation of files and directories within Dired buffers, making
+;; file navigation more intuitive and visually appealing.
 ;; https://github.com/wyuenho/all-the-icons-dired
 (package! all-the-icons-dired
   :recipe (:host github
            :repo "wyuenho/all-the-icons-dired"))
 
-(package! bookmark+
-  :recipe (:host github
-           :repo "emacsmirror/bookmark-plus"))
-
+;; With this package you can trivially convert a selected region of code
+;; to a screenshot, with nothing but Emacs itself.
 ;; https://github.com/tecosaur/screenshot
 (package! screenshot
   :recipe (:host github
@@ -96,37 +128,103 @@
            ;; https://github.com/tecosaur/screenshot/issues/11
            :build (:not compile)))
 
-(package! polymode)
-(package! poly-R)
+;; This is meant to be an extremely minimalist presentation tool for
+;; Emacs `org-mode'.
+;; https://github.com/rlister/org-present
+(package! org-present
+  :recipe (:host github
+           :repo "rlister/org-present"))
 
-;; https://github.com/zerolfx/copilot.el#example-for-doom-emacs
+;; Obsolete.
+;; (package! smartparens)      ;; See `:config (default +smartparens)'.
+;; (package! lsp-treemacs)     ;; Part of `:ui (treemacs +lsp)'.
+;; (package! auctex)           ;; Part of `:lang latex'.
+;; (package! yasnippet)        ;; Part of `:edit snippets'.
+
+;; R ///////////////////////////////////////////////////////////////////
+
+;; Polymode stack.
+;; (package! polymode)         ;; Part of `:lang ess'.
+;; (package! poly-R)           ;; Part of `:lang ess'.
+;; (package! markdown-mode)    ;; Part of `:lang markdown'.
+(package! poly-noweb)
+(package! poly-markdown)
+(package! quarto-mode)
+(package! orgalist)
+
+;; To do tidyverse-like view and manipulate data in ESS and R.
+;; https://github.com/ShuguangSun/ess-view-data
+(package! ess-view-data)
+
+;; Ease to read reports.
+;; https://codeberg.org/joostkremers/visual-fill-column
+(package! visual-fill-column)
+
+;; Gets an error about unbalanced expressions.
+;; (package! electric-spacing-r
+;;   :recipe (:host github
+;;            :repo "walmes/electric-spacing"
+;;            ;; :branch "master"
+;;            :files ("electric-spacing-r.el")
+;;            ))
+
+;; IA agents ///////////////////////////////////////////////////////////
+
+;; Copilot.el is an Emacs plugin for GitHub Copilot.
+;; https://github.com/copilot-emacs/copilot.el
 ;;
-;; Update NodeJS for Copilot.
+;; TODO Update NodeJS for Copilot.
 ;; $ curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 ;; $ sudo apt-get install -y nodejs
 (package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+  :recipe (:host github
+           :repo "zerolfx/copilot.el"
+           :files ("*.el" "dist")))
 
-;; https://codeium.com/blog/codeium-copilot-alternative-in-emacs
-;; https://codeium.com/emacs_tutorial
-;; https://github.com/Exafunction/codeium.el
-;; (package! codeium
-;;   :recipe (:host github :repo "Exafunction/codeium.el"))
+;; GPTel is a simple Large Language Model chat client for Emacs, with
+;; support for multiple models and backends. Backends includes
+;; `ChatGPT', `Gemini' and many others.
+;; https://github.com/karthink/gptel
+;;
+;; (package! gptel)
 
-(package! org-present
-  :recipe (:host github :repo "rlister/org-present"))
-
-(package! visual-fill-column)
-
-(package! xterm-color)
-
-;; (package! c3po
-;;   :recipe (:host github :repo "d1egoaz/c3po.el"))
-
+;; This Emacs Code extension allows you to use the official OpenAI API
+;; to generate code or natural language responses from OpenAI's ChatGPT
+;; to your questions, right within the editor.
+;; https://github.com/emacs-openai/chatgpt
+;;
 ;; (package! chatgpt
 ;;   :recipe (:host github :repo "emacs-openai/chatgpt"))
 
-;; (package! gptel)
+;; Codeium autocompletes your code with AI in all major IDEs. Is uses
+;; Codeium AI and its FREE for users.
+;; · https://codeium.com/blog/codeium-copilot-alternative-in-emacs
+;; · https://codeium.com/emacs_tutorial
+;; · https://github.com/Exafunction/codeium.el
+;;
+;; (package! codeium
+;;   :recipe (:host github :repo "Exafunction/codeium.el"))
+
+;; ChatGPT inside Emacs.
+;; https://github.com/d1egoaz/c3po.el
+;;
+;; (package! c3po
+;;   :recipe (:host github :repo "d1egoaz/c3po.el"))
+
+;; `gpt.el' is a simple Emacs package that lets you interact with
+;; instruction-following language models like ChatGPT and GPT-4 from
+;; your editor.
+;; https://github.com/stuhlmueller/gpt.el
+;;
+;; (package! gpt)
+
+;; Python //////////////////////////////////////////////////////////////
+
+;; Elpy is the Emacs Python Development Environment. It aims to provide
+;; an easy to install, fully-featured environment for Python
+;; development.
+;; https://elpy.readthedocs.io/en/latest/
+(package! elpy)
 
 ;; pyls.
 ;; pip install --user python-language-server[all]
@@ -139,28 +237,8 @@
 (package! lsp-python-ms :disable t) ;; Turn off.
 ;; (package! lsp-pyright)
 
-;; (package! exec-path-from-shell)
+;; Color themes ////////////////////////////////////////////////////////
 
-;; Needs `sudo apt-get install virtualenv'.
-;; (package! lsp-jedi)
-;; (package! jedi)
-
-;; Version with Org-struct.
-;; https://github.com/bzg/org-mode/releases/tag/release_9.1.14
-;; (package! org-mode
-;;   :pin "30498ef932bc35c26e3e58278f4987a67480b446"
-;;   :recipe (:host github
-;;            :repo "bzg/org-mode"))
-
-;; Gets an error about unbalanced expressions.
-;; (package! electric-spacing-r
-;;   :recipe (:host github
-;;            :repo "walmes/electric-spacing"
-;;            ;; :branch "master"
-;;            :files ("electric-spacing-r.el")
-;;            ))
-
-;; Color themes.
 ;; (package! afternoon-theme)
 ;; (package! apropospriate-theme)
 ;; (package! apropospriate-theme)
